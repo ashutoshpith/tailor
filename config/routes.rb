66b3794305bcd994cc9268cdc2b9c4daa11ae576
ashutosh <amxdp18@gmail.com>
2018-10-13 17:16:47 +0530

    simple form db creatio n

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :problems
   resources :models
   resources :companies
-  root 'films#index'
+  root 'repairs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
